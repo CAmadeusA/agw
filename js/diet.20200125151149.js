8b83dbaee0aa365a6824dbc13b2ccdc7401c3710
CAmadeusA <christianarchuleta@gmail.com>
2020-01-25 15:12:48 -0600

    Replace with G-Docs

--- js/diet.20200125151149.js
+++ js/diet.20200125151149.js
@@ -4,8 +4,7 @@ $.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof att
 var lwi=-1;function thresholdPassed(){var w=$(window).width();var p=false;var cw=0;if(w>=960){cw++;}if(w>=1200){cw++;}if(lwi!=cw){p=true;}lwi=cw;return p;}
 
 $(document).ready(function() {
-r=function(){if(thresholdPassed()){dpi=window.devicePixelRatio;if($(window).width()>=1200){$('.js-37').attr('src', 'images/car4-450.jpg');}else if($(window).width()>=960){$('.js-37').attr('src', 'images/car4-360-1.jpg');}else{$('.js-37').attr('src', (dpi>1) ? ((dpi>2) ? 'images/car4-360.jpg' : 'images/car4-240.jpg') : 'images/car4-120.jpg');}}};
+r=function(){if(thresholdPassed()){dpi=window.devicePixelRatio;if($(window).width()>=1200){$('.js-37').attr('src', 'images/car4-450-2.jpg');}else if($(window).width()>=960){$('.js-37').attr('src', 'images/car4-360-6.jpg');}else{$('.js-37').attr('src', (dpi>1) ? 'images/car4-552-4.jpg' : 'images/car4-276-4.jpg');}}};
 if(!window.HTMLPictureElement){$(window).resize(r);r();}
-$('form').h5Validate();setTimeout(function(){$('.js-38').prop('disabled',false);$("input[name='jvqqva']").attr('value', 'wpeyehtugordvsyn');},1000);
 
 });
